3f06775083e5447846b4b28b11c73a80272a85ad
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2022-04-05 01:47:47 +0200

    task: Index page

--- server.py
+++ server.py
@@ -1,1 +1,20 @@
-# put your code here
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    planets = get_planets()
+    return render_template('index.html', title='Star Wars universe planets', planets=planets)
+
+
+
+def get_planets():
+    url = 'https://swapi.dev/api/planets/'
+    response = requests.get(url)
+    return response.json()
+
+if __name__ == '__main__':
+    app.run()
