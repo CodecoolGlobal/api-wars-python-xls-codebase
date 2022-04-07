44e0dc2499468c854fc038045db7f465f96ea113
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2022-04-07 22:08:10 +0200

    task Index page: added list-style table for mobile

--- static/js/residents.js
+++ static/js/residents.js
@@ -1,4 +1,45 @@
 $(function() {
+    addLabels()
+    mobileTableCheck();
+    $(window).on('resize', mobileTableCheck);
+
+    function addLabels() {
+        let labels = [];
+        $('#planet-table th').each(function() {
+            labels.push($(this).html())
+        })
+        $('#planet-table tr').each( function() {
+            $(this).find('td').not(':last').each(function (i) {
+                $(this).prepend('<span class="label">' + labels[i] + ': </span>')
+            })
+        })
+        // $('#planet-table .label').hide()
+    }
+
+    function mobileTableCheck() {
+        if (window.matchMedia("only screen and (max-width: 576px)").matches) {
+            $('#planet-table tr').addClass('row');
+            $('#planet-table th, #planet-table td').addClass('col-');
+            $('#planet-table').closest('div').removeClass('table-responsive')
+            $('#planet-table thead').hide()
+            $('#planet-table .label').show()
+            $('.pagination').addClass('d-flex justify-content-between')
+            $('.pagination .btn').addClass('col-5')
+            $('#planet-table tr td:last-child .btn').addClass('d-grid')
+            $('#planet-table > :not(:first-child)').addClass('no-border')
+        } else {
+            $('#planet-table tr').removeClass('row');
+            $('#planet-table th, #planet-table td').removeClass('col-');
+            $('#planet-table').closest('div').addClass('table-responsive')
+            $('#planet-table thead').show()
+            $('#planet-table .label').hide()
+            $('.pagination').removeClass('d-flex justify-content-between')
+            $('.pagination .btn').removeClass('col-5')
+            $('#planet-table tr td:last-child .btn').removeClass('d-grid')
+            $('#planet-table > :not(:first-child)').removeClass('no-border')
+        }
+    }
+
     $('.btn-residents').on( 'click', event => {
         event.preventDefault();
         let planetId = $(event.target).data('planet_id')
