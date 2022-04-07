ae564f3461602997319addf8bf20591a69c7ac78
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2022-04-07 22:50:16 +0200

    Refactor

--- static/js/residents.js
+++ static/js/residents.js
@@ -1,52 +1,59 @@
 $(function() {
+    init();
+});
+
+
+function init() {
     addLabels()
     mobileTableCheck();
     $(window).on('resize', mobileTableCheck);
+    $('body').prepend(modalHTML)
+    ajaxButtons()
+}
 
-    function addLabels() {
-        let labels = [];
-        $('#planet-table th').each(function() {
-            labels.push($(this).html())
-        })
-        $('#planet-table tr').each( function() {
-            $(this).find('td').not(':last').each(function (i) {
-                $(this).prepend('<span class="label">' + labels[i] + ': </span>')
-            })
-        })
-        // $('#planet-table .label').hide()
-    }
-
-    function mobileTableCheck() {
-        if (window.matchMedia("only screen and (max-width: 576px)").matches) {
-            $('#planet-table tr').addClass('row');
-            $('#planet-table th, #planet-table td').addClass('col-');
-            $('#planet-table').closest('div').removeClass('table-responsive')
-            $('#planet-table thead').hide()
-            $('#planet-table .label').show()
-            $('.pagination').addClass('d-flex justify-content-between')
-            $('.pagination .btn').addClass('col-5')
-            $('#planet-table tr td:last-child .btn').addClass('d-grid')
-            $('#planet-table > :not(:first-child)').addClass('no-border')
-        } else {
-            $('#planet-table tr').removeClass('row');
-            $('#planet-table th, #planet-table td').removeClass('col-');
-            $('#planet-table').closest('div').addClass('table-responsive')
-            $('#planet-table thead').show()
-            $('#planet-table .label').hide()
-            $('.pagination').removeClass('d-flex justify-content-between')
-            $('.pagination .btn').removeClass('col-5')
-            $('#planet-table tr td:last-child .btn').removeClass('d-grid')
-            $('#planet-table > :not(:first-child)').removeClass('no-border')
-        }
-    }
-
-    $('.btn-residents').on( 'click', event => {
+function ajaxButtons() {
+   $('.btn-residents').on( 'click', event => {
         event.preventDefault();
         let planetId = $(event.target).data('planet_id')
         showResidents(planetId)
     })
-    $('body').prepend(modalHTML)
-});
+}
+
+function addLabels() {
+    let labels = [];
+    $('#planet-table th').each(function() {
+        labels.push($(this).html())
+    })
+    $('#planet-table tr').each( function() {
+        $(this).find('td').not(':last').each(function (i) {
+            $(this).prepend('<span class="label">' + labels[i] + ': </span>')
+        })
+    })
+}
+
+function mobileTableCheck() {
+    if (window.matchMedia("only screen and (max-width: 576px)").matches) {
+        $('#planet-table tr').addClass('row');
+        $('#planet-table th, #planet-table td').addClass('col-');
+        $('#planet-table').closest('div').removeClass('table-responsive')
+        $('#planet-table thead').hide()
+        $('#planet-table .label').show()
+        $('.pagination').addClass('d-flex justify-content-between')
+        $('.pagination .btn').addClass('col-5')
+        $('#planet-table tr td:last-child .btn').addClass('d-grid')
+        $('#planet-table > :not(:first-child)').addClass('no-border')
+    } else {
+        $('#planet-table tr').removeClass('row');
+        $('#planet-table th, #planet-table td').removeClass('col-');
+        $('#planet-table').closest('div').addClass('table-responsive')
+        $('#planet-table thead').show()
+        $('#planet-table .label').hide()
+        $('.pagination').removeClass('d-flex justify-content-between')
+        $('.pagination .btn').removeClass('col-5')
+        $('#planet-table tr td:last-child .btn').removeClass('d-grid')
+        $('#planet-table > :not(:first-child)').removeClass('no-border')
+    }
+}
 
 function showResidents(planetId) {
     url = '/planet/' + planetId + '/residents'
