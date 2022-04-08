777ca5defcb15fe8619b404f7ea2b523c94ef9be
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2022-04-08 12:34:48 +0200

    usability: added Spinner to residents.popup

--- static/js/residents.js
+++ static/js/residents.js
@@ -56,31 +56,40 @@ function mobileTableCheck() {
 }
 
 function showResidents(planetId) {
+    var myModal = new bootstrap.Modal(document.getElementById('residents-modal'))
+    $('#residents-modal .modal-body').html(spinner)
+    myModal.show()
     url = '/planet/' + planetId + '/residents'
     $.get(url, function (data) {
         let title = $(data).find('h2').html();
         let table = $(data).find('.table-responsive');
         $('.modal-title').html(title)
         $('.modal-body').html(table);
-        var myModal = new bootstrap.Modal(document.getElementById('residents-modal'))
-        myModal.show()
   })
 }
 
 modalHTML = `
-    <div id="residents-modal" class="modal" tabindex="-1">
-      <div class="modal-dialog modal-xl">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title"></h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          </div>
-        </div>
+<div id="residents-modal" class="modal" tabindex="-1">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title"></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
+  </div>
+</div>
+`
+spinner = `
+<div class="d-flex justify-content-center">
+<div class="d-flex justify-content-center">
+  <div class="spinner-border text-primary" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div>
 `
