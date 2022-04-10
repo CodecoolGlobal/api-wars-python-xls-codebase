24aa508256f4512609b3a0736e36957f7f94c31e
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2022-04-10 23:03:51 +0200

    task Login system: User registration scribbling

--- server.py
+++ server.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, redirect, session, jsonify
 import requests
 from urllib.parse import urlparse
+from forms import LoginForm
+
+import os
+import psycopg2
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        database=os.environ.get('DB_DATABASE_NAME'),
+        user=os.environ.get('DB_USERNAME'),
+        password=os.environ.get('DB_PASSWORD'),
+        host=os.environ.get('DB_HOST'))
+    return conn
 
 app = Flask(__name__)
 
@@ -8,12 +20,12 @@ BASE_URL = 'https://swapi.dev/api'
 PLANETS = 'planets'
 
 
+
 @app.route('/')
 def index():
     planets_raw_data = get_raw_planets()
     planet_data = get_planets(planets_raw_data)
     pagination = get_pagination(planets_raw_data)
-
     return render_template('index.html', title='Star Wars universe planets', planets=planet_data, pagination=pagination)
 
 
@@ -22,7 +34,6 @@ def next_page(page_id):
     planets_raw_data = get_raw_planets('?page={}'.format(page_id))
     planet_data = get_planets(planets_raw_data)
     pagination = get_pagination(planets_raw_data)
-
     return render_template('index.html', title='Star Wars universe planets', planets=planet_data, pagination=pagination)
 
 
@@ -34,6 +45,26 @@ def residents(planet_id):
         response = requests.get(url).json()
         residents.append(response)
     return render_template('residents.html', title='Star Wars universe planets', planet=planet_raw_data, residents=residents)
+
+
+@app.route('/user-registration', methods=['GET', 'POST'])
+def user_registration():
+    form = LoginForm(request.form)
+
+    if request.method == 'POST' and form.validate():  # form.validate_on_submit()
+        username = form.username
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO users (username, password) VALUES (%s, %s)', (form.username.data, form.password.data))
+        conn.commit()
+        cur.close()
+        conn.close()
+
+    return render_template('registration.html', title='Star Wars universe planets - User registration', form=form)
+
+
+
+
 
 
 def get_raw_planets(parameter=''):
